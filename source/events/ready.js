cecf352d576ae3b5d2de90844af6fbd71af97205
PapaSnags <58458169+LachlanDev@users.noreply.github.com>
2022-11-12 14:47:02 +1100

    moved old v12 files

--- source/events/ready.js
+++
@@ -1,5 +0,0 @@
-const chalk = require("chalk")
-const config = require("../cfg/config.json")
-module.exports = (client) => {
-    console.log(chalk.magenta(`Bot Made by LachlanDev#8014 \nPrefix is ${config.prefix}`));
-  }
