dc3bce2433953cdb458d61781c90a82f9fbd5bec
PapaSnags <58458169+LachlanDev@users.noreply.github.com>
2022-11-12 17:01:59 +1100

    Create ready.js

---
+++ src/events/ready.js
@@ -0,0 +1,28 @@
+module.exports = {
+    name: 'ready',
+    once: true,
+    async execute(client) {
+        console.log('Ready!');
+
+        async function pickPresence () {
+            const option = Math.floor(Math.random() * statusArray.length);
+
+            try {
+                await client.user.setPresence({
+                    activities: [
+                        {
+                            name: statusArray[option].content,
+                            type: statusArray[option].type,
+
+                        },
+                    
+                    ],
+
+                    status: statusArray[option].status
+                })
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    },
+};
