57d7eda42019dd1e3c386bbb1b3e77dafe65acfb
PapaSnags <58458169+LachlanDev@users.noreply.github.com>
2025-05-04 23:37:41 +1000

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,28 +1,20 @@
-const { Interaction } = require("discord.js");
-
-module.exports = {
+export default {
     name: 'interactionCreate',
-    async execute(interaction, client) {
-        if (!interaction.isCommand()) return;
-
-        const command = client.commands.get(interaction.commandName);
-
-        if (!command) return
-        
-        try{
-
-
-            await command.execute(interaction, client);
-        } catch (error) {
-            console.log(error);
-            await interaction.reply({
-                content: 'There was an error while executing this command!', 
-                ephemeral: true
-            });
-        } 
-
-    },
-    
-
-
-};
+    async execute(interaction) {
+      if (!interaction.isChatInputCommand()) return;
+  
+      const command = interaction.client.commands.get(interaction.commandName);
+      if (!command) return;
+  
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({
+          content: '❌ There was an error executing this command.',
+          ephemeral: true
+        });
+      }
+    }
+  };
+  
